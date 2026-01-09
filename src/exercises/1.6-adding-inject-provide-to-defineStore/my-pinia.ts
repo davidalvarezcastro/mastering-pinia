887027ecbc9175bf57ff38602e514f9c7f082b32
David Alvarez <davalv@televes.com>
2026-01-09 19:07:11 +0100

    solved adding inject & provide to defineStore

--- src/exercises/1.6-adding-inject-provide-to-defineStore/my-pinia.ts
+++ src/exercises/1.6-adding-inject-provide-to-defineStore/my-pinia.ts
@@ -1,11 +1,14 @@
-import { Plugin, effectScope } from 'vue'
-
-const stores = new WeakMap<() => unknown, unknown>()
-
-const globalEffect = effectScope(true)
+import { EffectScope, InjectionKey, Plugin, effectScope, inject } from 'vue'
 
 export function defineStore<R>(fn: () => R) {
   function useStore() {
+    const globalEffect = inject(effectKey)
+    const stores = inject(storesMapKey)
+
+    if (!globalEffect || !stores) {
+      throw new Error('You must use useStore() inside a setup function!')
+    }
+
     if (!stores.has(fn)) {
       const store = globalEffect
         .run(() =>
@@ -23,7 +26,14 @@ export function defineStore<R>(fn: () => R) {
   return useStore
 }
 
+const effectKey = Symbol('effect') as InjectionKey<EffectScope>
+const storesMapKey = Symbol('stores') as InjectionKey<Map<() => unknown, unknown>>
+
 // Keep this exported
 export const appPlugin: Plugin = app => {
-  // ...
+  const stores = new WeakMap<() => unknown, unknown>()
+  const globalEffect = effectScope(true)
+
+  app.provide(storesMapKey, stores)
+  app.provide(effectKey, globalEffect)
 }
