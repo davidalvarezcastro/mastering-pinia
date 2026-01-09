3020285a58423b6fca832108c840cedf295b19af
David Alvarez <davalv@televes.com>
2026-01-09 18:35:11 +0100

    solved implementing our own defineStore exercise

--- src/exercises/1.4-implementing-our-own-defineStore/my-pinia.ts
+++ src/exercises/1.4-implementing-our-own-defineStore/my-pinia.ts
@@ -1,3 +1,17 @@
-export function defineStore() {
-  // TODO: see instructions.md
+import { effectScope } from 'vue'
+
+const stores = new WeakMap<() => unknown, unknown>() // key/value ignored by garbage collector
+const globalEffect = effectScope(true)
+
+export function defineStore<R>(fn: () => R) {
+  function useStore() {
+    if (!stores.has(fn)) {
+      const store = globalEffect.run(() => effectScope().run(fn))
+      stores.set(fn, store)
+    }
+
+    return stores.get(fn) as R
+  }
+
+  return useStore
 }
